35040d8721a478281a043c599e3e55ac3bf22ae8
Ryo Yamada <r.yamada@variosecure.net>
2020-01-07 20:07:04 +0900

    add new module and fix index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-exports.sample = (req, res) => {
-    console.log(req)
-    res.status(200).send('Hello World')
+const sample = (req, res) => {
+    res.send('Hello Next World')
+}
+exports.function = (req, res) => {
+    const paths = {
+        '/sample': sample,
+        '/': () => res.send(Object.keys(paths)),
+    };
+    for (const [path, route] of Object.entries(paths)) {
+        if (req.path.startsWith(path)) {
+            return route(req, res);
+        }
+    };
+    res.send('No path found');
 }
